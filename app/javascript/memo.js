651587f2d17fab7ee50f5e976fa1c28c166027b9
satoba-30 <august03alone@gmail.com>
2021-03-24 19:42:29 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,14 +1,14 @@
-function post(){
-  const submit=document.getElementById("submit");
-  submit.addEventListener("click",(e)=>{
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
    e.preventDefault();
-  const form=document.getElementById("form");
-  const formData = new FormDate(form);
-  const XHR=new XMLHttpRequest();
-  XHR.open("POST","/posts",true);
-  XHR.responseType="json";
+  const form = document.getElementById("form");
+  const formData = new FormData(form);
+  const XHR = new XMLHttpRequest();
+  XHR.open("POST", "/posts", true);
+  XHR.responseType = "json";
   XHR.send(formData);
   });
-};
+} 
 
-window.addEventListener('load',post);
+window.addEventListener('load', post);
